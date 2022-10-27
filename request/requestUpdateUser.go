7302107f8b66f07db4390f407c9635fa09cf707f
Yayan-SRA <yayansra14@gmail.com>
2022-10-27 12:27:34 +0700

    show & edit

--- request/requestUpdateUser.go
+++ request/requestUpdateUser.go
@@ -1,9 +1,13 @@
 package request
 
+import (
+	"github.com/google/uuid"
+)
+
 type RequestUpdateUser struct {
-	ID       uint64 `json:"id" form:"id"`
-	UserName string `json:"userName" form:"userName" binding:"required"`
-	Name     string `json:"name" form:"name" binding:"required"`
-	Password string `json:"password,omitempty" form:"password,omitempty"`
-	Phone    string `json:"phone" binding:"required,phone"`
+	ID       uuid.UUID `json:"id" form:"id"`
+	UserName string    `json:"userName" form:"userName" binding:"required"`
+	Name     string    `json:"name" form:"name" binding:"required"`
+	Password string    `json:"password,omitempty" form:"password,omitempty"`
+	Phone    string    `json:"phone" binding:"required,phone"`
 }
