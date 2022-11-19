e4f793ae87f0011724da3ef5d784af351c25a8d5
aldisaputra17 <aldisaput17@gmail.com>
2022-11-19 18:32:57 +0700

    stop db

--- database/dbconnection.go
+++ database/dbconnection.go
@@ -6,16 +6,16 @@ import (
 	"time"
 
 	"github.com/aldisaputra17/dapur-fresh-id/entities"
-	"github.com/joho/godotenv"
+	// "github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func ConnectDB() *gorm.DB {
-	errEnv := godotenv.Load()
-	if errEnv != nil {
-		panic("Failed to load env file")
-	}
+	// errEnv := godotenv.Load()
+	// if errEnv != nil {
+	// 	panic("Failed to load env file")
+	// }
 
 	dbUser := os.Getenv("DB_USER")
 	dbPass := os.Getenv("DB_PASSWORD")
